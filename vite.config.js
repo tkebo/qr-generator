0c4bfdaf581fdb6705248046a641c005e7f0cb6f
MrFreeman <tkebo@example.com>
2025-04-03 02:24:08 +0400

    გასწორებულია import გზა index.html-ში

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: 'public', // აქედან ეძებს index.html-ს
   build: {
     outDir: '../dist',
     emptyOutDir: true,
